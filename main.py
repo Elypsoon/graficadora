f674ea6916c2a860dcb820ce75c7735cd447e4de
Angel <angel.gtz.pc.527@gmail.com>
2024-05-02 00:09:22 -0600

    Base de inputs hecho

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import figuras
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg #Permite integrar matplotlib con tkinter
 import tkinter as tk
+import tkinter.font as tkFont
+
 
 #Evita que el bucle se mantenga en ejecución
 def cerrar():
@@ -13,7 +15,7 @@ def cerrar():
 # Crea una ventana
 root = tk.Tk()
 root.title("Graficadora")
-root.wm_minsize(width=1200, height=1200)
+root.wm_minsize(width=1000, height=1000)
 
 # Crea una figura vacía inicial, ax es el contenedor de los elementos gráficos
 fig, ax = plt.subplots()
@@ -41,6 +43,19 @@ canvas.draw()
 
 # Inserta el lienzo en la ventana
 canvas.get_tk_widget().pack(expand=True, fill=tk.BOTH)
+
+#Para el desplegable
+select = tk.StringVar(value="Cuadrado")
+opciones = ["Cuadrado", "Rectángulo", "Triángulo", "Círculo"]
+desplegable = tk.OptionMenu(root, select, *opciones)
+desplegable.config(font=tkFont.Font(family="Helvetica", size=20, weight="bold"))  # Aplicar la fuente al OptionMenu
+desplegable.pack(anchor='w', padx=10, pady=10)
+
+#Inputs para el punto inicial
+puntoLabel_Cuadrado = tk.Label(root, text="Punto de partida:", font=tkFont.Font(family="Helvetica", size=20, weight="bold"))
+puntoLabel_Cuadrado.pack(side='left', padx=10, pady=10)
+puntoInput_Cuadrado = tk.Entry(root, width=30, font=tkFont.Font(family="Helvetica", size=20))
+puntoInput_Cuadrado.pack(side='left', padx=10, pady=10)
 
 # Ejecutar el bucle de eventos de Tkinter
 root.protocol("WM_DELETE_WINDOW", cerrar)
